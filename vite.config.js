858376f0c7c57a1807bee6de984049ad8477fa67
Huy Huynh <htqhuy1762@gmail.com>
2024-07-01 11:21:38 +0700

    Change Port

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  resolve: {
-      alias: {
-          // Định nghĩa alias cho ~
-          '~': '/src',
-      },
-  },
+    plugins: [react()],
+    resolve: {
+        alias: {
+            // Định nghĩa alias cho ~
+            '~': '/src',
+        },
+    },
+    server: {
+        port: 5175,
+    },
 });
